f14371b45e2a517baa4045efb9231161b7359d67
Renato <renato92.hora@gmail.com>
2024-09-16 21:15:28 -0300

    about us section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
-import logo from "../assets/logo-header/header-logo.png";
+import logo from "../assets/header/logo-header/header-logo.png";
 {/* import instagramIcon from "../assets/instagram-icon.png"; */}
 
 const Header = () => {
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,35 @@
 import React from 'react';
-import './Home.css';
+import './Home.css'; // Importa o arquivo de estilo CSS
+import spaImage from '../assets/home/main_video/mainphoto.png'; // Importa a imagem para ser usada no componente
+import placeholder from '../assets/home/placeholder/placeholder.png';
 
 const Home = () => {
     return (
         <div>
-            <div className="horizontal-line"></div>
             <main>
-                <section>
-                    <h2>Relaxe e Revitalize</h2>
-                    <p>Oferecemos os melhores serviços de spa para relaxamento e bem-estar.</p>
-                </section>
+                {/* Adicionando a imagem no centro da tela */}
+                <div className="video-container">
+                    <img src={spaImage} alt="Serviço de Spa" /> {/* Exibe a imagem importada com texto alternativo */}
+                </div>
+                <div className="about-us-container">
+                    {/* Texto e botão à esquerda */}
+                    <div className="about-us-text-container">
+                        <h2>Conheça nossa História</h2>
+                        <p>Oferecemos os melhores serviços de spa para relaxamento e bem-estar. Descubra a nossa jornada e o compromisso com o seu bem-estar.</p>
+                        <button>Saiba Mais</button>
+                    </div>
+
+                    {/* Imagem à direita */}
+                    <div className="about-us-image-container">
+                        <img src={placeholder} alt="Serviço de Spa" />
+                    </div>
+                </div>
             </main>
             <footer>
-                <p>&copy; 2024 SPA Beleza. Todos os direitos reservados.</p>
+                <p>&copy; 2024 SPA Beleza. Todos os direitos reservados.</p> {/* Texto de rodapé com os direitos reservados */}
             </footer>
         </div>
     );
 };
 
-export default Home;
+export default Home; // Exporta o componente Home para ser usado em outros lugares
