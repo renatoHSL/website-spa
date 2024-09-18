ec0607525998bddf693fd8922e02dbfba7020576
Renato <renato92.hora@gmail.com>
2024-09-18 14:51:20 -0300

    feat: stick header, map location, bottom lines

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -80,7 +80,7 @@ const Home = () => {
                 <div className="map-container">
                     <iframe
                         title="Localização do SPA" /* Adiciona um título ao iframe para acessibilidade */
-                        src="https://www.google.com/maps/embed?pb=..."
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3154.6261234446984!2d-43.234223699995525!3d-22.925689899991152!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjLCsDU1JzMyLjUiUyA0M8KwMTQnMDMuMiJX!5e0!3m2!1spt-BR!2sbr!4v1630461013475!5m2!1spt-BR!2sbr"
                         width="100%"
                         height="450"
                         style={{ border: "0" }} /* Certifique-se de usar um objeto para a prop 'style' */
